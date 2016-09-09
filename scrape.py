a415735c979fe6a42f22d471550f9bd291261387
Andrew Chavez <adchavez@gmail.com>
2016-09-09 13:28:28 -0500

    Parse datetimes

--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import logging
 import os
@@ -25,6 +26,14 @@ def get_config():
     return json.loads(config)
 
 
+def parse_time(time_str):
+    """Parse an ISO-8601 timestamp into a Python datetime"""
+    try:
+        return datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S.%fZ')
+    except ValueError:
+        return datetime.strptime(time_str, '%Y-%m-%dT%H:%M:%S.%f')
+
+
 def scrapeActiveCalls(*args):
     """Scrape active calls and save them into our database"""
     config = get_config()
@@ -38,6 +47,12 @@ def scrapeActiveCalls(*args):
     r.raise_for_status()
 
     for active_call in r.json():
+        parsed_dates = {
+            'date_time_dt': parse_time(active_call['date_time']),
+            ':created_at_dt': parse_time(active_call[':created_at']),
+            ':updated_at_dt': parse_time(active_call[':updated_at'])
+        }
+        active_call.update(**parsed_dates)
         calls_table.upsert(active_call, ['incident_number'])
 
     num_added = calls_table.count() - original_count
